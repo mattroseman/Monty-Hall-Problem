e448fb4d03ec82ff1a69e358ecdd9c0ffc472195
Matthew Roseman <mroseman95@gmail.com>
2017-01-28 04:49:32 +0000

    added some of the techniques in rand-test over to monty-hall.c
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- monty-hall.c
+++ monty-hall.c
@@ -2,19 +2,50 @@
 #include <stdlib.h>
 
 void monty_hall() {
-    void *random_byte = malloc(1);
+    unsigned char *random_byte = malloc(1);
 
     FILE *f;
     if ((f = fopen("/dev/urandom", "rb")) == NULL) {
         fprintf(stderr, "There was a problem opening urandom.\n");
         exit(-1);
     }
-    fread(&random_byte, sizeof(random_byte), 1, f);
-    fclose(f);
+
+    // PICK PRIZE DOOR
+    do {
+        fread(random_byte, sizeof(random_byte), 1, f);
+    } while (*random_byte == 0xFF);
 
     // initialize doors with cars and goats
     // 3 possible states
-    // 
+    // byte between 0x00 and 0x54
+    if (*random_byte <= 0x54) {
+        // first door has car
+    // byte between 0x55 and 0xA9
+    } else if (*random_byte <= 0xA9) {
+        // second door has car
+    // byte between 0xAA and 0xFE
+    } else {
+        // third door has car
+    }
+
+    // PICK FIRST DOOR GUESS
+    do {
+        fread(random_byte, sizeof(random_byte), 1, f);
+    } while (*random_byte == 0xFF);
+
+    // place the first door guess
+    // byte between 0x00 and 0x54
+    if (*random_byte <= 0x54) {
+        // guess the first door
+    // byte between 0x55 and 0xA9
+    } else if (*random_byte <= 0xA9) {
+        // guess the second door
+    // byte between 0xAA and 0xFE
+    } else {
+        // guess the thrid door
+    }
+
+    fclose(f);
 }
 
 int main(int argc, char *argv[]) {
