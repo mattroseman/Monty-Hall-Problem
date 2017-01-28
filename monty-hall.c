7a873003b6e220477dd88c71afe4c4844b69bb86
Matthew Roseman <mroseman95@gmail.com>
2017-01-28 23:34:29 +0000

    monty hall is complete and shows that ifyou switch you get roughly 2/3 chance of winning
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- monty-hall.c
+++ monty-hall.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-void monty_hall() {
+bool monty_hall() {
     unsigned char *random_byte = malloc(1);
+    int prize_door;
+    bool win_value;
 
     FILE *f;
     if ((f = fopen("/dev/urandom", "rb")) == NULL) {
@@ -20,12 +23,15 @@ void monty_hall() {
     // byte between 0x00 and 0x54
     if (*random_byte <= 0x54) {
         // first door has car
+        prize_door = 1;
     // byte between 0x55 and 0xA9
     } else if (*random_byte <= 0xA9) {
         // second door has car
+        prize_door = 2;
     // byte between 0xAA and 0xFE
     } else {
         // third door has car
+        prize_door = 3;
     }
 
     // PICK FIRST DOOR GUESS
@@ -37,19 +43,56 @@ void monty_hall() {
     // byte between 0x00 and 0x54
     if (*random_byte <= 0x54) {
         // guess the first door
+        // if the guess is correct
+        if (prize_door == 1) {
+            // switching wrong
+            // staying right
+            win_value = false;
+        // if the guess is wrong
+        } else {
+            // switching right
+            // staying wrong
+            win_value = true;
+        }
     // byte between 0x55 and 0xA9
     } else if (*random_byte <= 0xA9) {
         // guess the second door
+        // if the guess is correct
+        if (prize_door == 2) {
+            // switching wrong
+            // staying right
+            win_value = false;
+        // if the guess is wrong
+        } else {
+            // switching right
+            // staying wrong
+            win_value = true;
+        }
     // byte between 0xAA and 0xFE
     } else {
         // guess the thrid door
+        // if the guess is correct
+        if (prize_door == 3) {
+            // switching wrong
+            // staying right
+            win_value = false;
+        // if the guess is wrong
+        } else {
+            // switching right
+            // staying wrong
+            win_value = true;
+        }
     }
 
     fclose(f);
+    return win_value;
 }
 
 int main(int argc, char *argv[]) {
     long num_tests;
+    int num_correct = 0;
+    int num_wrong = 0;
+
     if (argc == 2) {
         char *end;
         if ((num_tests = strtol(argv[1], &end, 10)) == 0) {
@@ -62,7 +105,13 @@ int main(int argc, char *argv[]) {
     }
 
     for (int i=0; i < num_tests; i++) {
-        monty_hall();
+        if (monty_hall()) {
+            num_correct++;
+        } else {
+            num_wrong++;
+        }
+
+        printf("Wins: %d Losses: %d Win Percent %lf\n", num_correct, num_wrong, (double)num_correct/(num_correct+num_wrong));
     }
     return 0;
 }
